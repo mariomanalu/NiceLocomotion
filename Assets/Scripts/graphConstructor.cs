9f03e18d286661e882c0c342ba94fb665db76999
Austin Yu <48722823+mariomanalu@users.noreply.github.com>
2022-03-14 10:36:37 -0500

    Instantiate a node on button click

--- Assets/Scripts/graphConstructor.cs
+++ Assets/Scripts/graphConstructor.cs
@@ -10,9 +10,13 @@ public class graphConstructor : MonoBehaviour
     [SerializeField] 
     private InputActionAsset actionAsset;
 
+    [SerializeField]
+    Transform cameraTransform;
+
     UnityEngine.XR.InputDevice controller;
 
     private bool buttonPressed;
+    private bool button;
 
     [SerializeField]
     GameObject nodePrefab;
@@ -34,25 +38,28 @@ public class graphConstructor : MonoBehaviour
         {
         }
 
-        // RaycastHit hit;
         // Ray targetRay = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, cameraTransform.forward * 5, Color.red);
 
-        // if(Physics.Raycast(targetRay, out hit, distance))
-        // {
-        //     Debug.Log(hit.transform.position);
-        // }
-
-        if (controller.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out buttonPressed) && buttonPressed)
+        
+        if (controller.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out button) && button)
         {
-            Debug.Log("READ");
-            if (nodePrefab)
+            if (!buttonPressed)
+            { // JoystickDown boolean is used to catch the down event so that this code is not called every frame the button is pressed
+                buttonPressed = true;
+            }
+        }
+        else
+        {
+            if (buttonPressed)
             {
-                Debug.Log("nodePrefab is not null");
+                Debug.Log("Instantiating one node");
+                Instantiate(nodePrefab, cameraTransform.position + (cameraTransform.forward * 5), Quaternion.identity);
+                buttonPressed = false; // When the joystick click is let go then this is set back to false so that it can catch the first click again
             }
 
-            
-            Instantiate(nodePrefab, new Vector3(0,0,0), Quaternion.identity);
         }
+        
         
     }
 }
